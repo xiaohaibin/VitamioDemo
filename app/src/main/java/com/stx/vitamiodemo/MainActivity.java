8c6f090b0109bdd60bf34bd29886feda7df2ed88
Mr.xiao <xhb_199409@163.com>
2016-06-29 23:57:06 +0800

    Vitamio视频播放器demo，自定义视频控制器界面

---
+++ app/src/main/java/com/stx/vitamiodemo/MainActivity.java
@@ -0,0 +1,123 @@
+package com.stx.vitamiodemo;
+
+import android.content.res.Configuration;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import io.vov.vitamio.LibsChecker;
+import io.vov.vitamio.MediaPlayer;
+import io.vov.vitamio.Vitamio;
+import io.vov.vitamio.widget.MediaController;
+import io.vov.vitamio.widget.VideoView;
+
+/**
+ * Vitamio视频播放框架Demo
+ */
+public class MainActivity extends AppCompatActivity implements MediaPlayer.OnInfoListener, MediaPlayer.OnBufferingUpdateListener{
+    //视频地址
+//    private String path = "http://baobab.wdjcdn.com/145076769089714.mp4";
+    private String path = "http://xiaoyuyu.oss-cn-shenzhen.aliyuncs.com/iapp/%E5%B9%BF%E5%91%8A.avi";
+    private Uri uri;
+    private ProgressBar pb;
+    private TextView downloadRateView, loadRateView;
+    private MediaController mMediaController;
+    private CustomMediaController mCustomMediaController;
+    private VideoView mVideoView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //定义全屏参数
+        int flag = WindowManager.LayoutParams.FLAG_FULLSCREEN;
+        //获得当前窗体对象
+        Window window = MainActivity.this.getWindow();
+        //设置当前窗体为全屏显示
+        window.setFlags(flag, flag);
+        //必须写这个，初始化加载库文件
+        Vitamio.initialize(this);
+        //设置视频解码监听
+        if (!LibsChecker.checkVitamioLibs(this)) {
+            return;
+        }
+        setContentView(R.layout.activity_main);
+        initView();
+        initData();
+    }
+
+    //初始化控件
+    private void initView() {
+        mVideoView = (VideoView) findViewById(R.id.buffer);
+        mMediaController= new MediaController(this);
+        mCustomMediaController=new CustomMediaController(this,mVideoView,this);
+        mCustomMediaController.setVideoName("白火锅 x 红火锅");
+        pb = (ProgressBar) findViewById(R.id.probar);
+        downloadRateView = (TextView) findViewById(R.id.download_rate);
+        loadRateView = (TextView) findViewById(R.id.load_rate);
+    }
+
+    //初始化数据
+    private void initData() {
+        uri = Uri.parse(path);
+        mVideoView.setVideoURI(uri);//设置视频播放地址
+        mVideoView.setMediaController(mCustomMediaController);
+        mVideoView.setVideoQuality(MediaPlayer.VIDEOQUALITY_HIGH);//高画质
+        mMediaController.show(5000);
+        mVideoView.requestFocus();
+        mVideoView.setOnInfoListener(this);
+        mVideoView.setOnBufferingUpdateListener(this);
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.setPlaybackSpeed(1.0f);
+            }
+        });
+    }
+
+    @Override
+    public boolean onInfo(MediaPlayer mp, int what, int extra) {
+        switch (what) {
+            case MediaPlayer.MEDIA_INFO_BUFFERING_START:
+                if (mVideoView.isPlaying()) {
+                    mVideoView.pause();
+                    pb.setVisibility(View.VISIBLE);
+                    downloadRateView.setText("");
+                    loadRateView.setText("");
+                    downloadRateView.setVisibility(View.VISIBLE);
+                    loadRateView.setVisibility(View.VISIBLE);
+
+                }
+                break;
+            case MediaPlayer.MEDIA_INFO_BUFFERING_END:
+                mVideoView.start();
+                pb.setVisibility(View.GONE);
+                downloadRateView.setVisibility(View.GONE);
+                loadRateView.setVisibility(View.GONE);
+                break;
+            case MediaPlayer.MEDIA_INFO_DOWNLOAD_RATE_CHANGED:
+                downloadRateView.setText("" + extra + "kb/s" + "  ");
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public void onBufferingUpdate(MediaPlayer mp, int percent) {
+        loadRateView.setText(percent + "%");
+    }
+
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        //屏幕切换时，设置全屏
+        if (mVideoView != null){
+            mVideoView.setVideoLayout(VideoView.VIDEO_LAYOUT_SCALE, 0);
+        }
+        super.onConfigurationChanged(newConfig);
+    }
+
+}
